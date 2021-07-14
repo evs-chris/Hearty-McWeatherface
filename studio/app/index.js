b23446227d124395ef808cbf8482bb4969736a16
Chris Reeves <chris@ev-soft.net>
2021-07-14 02:35:22 -0400

    fix max water level limit so the bubble doesn't float off the bar

--- studio/app/index.js
+++ studio/app/index.js
@@ -1119,7 +1119,7 @@ function updateStats() {
       const w = ui.stats.water;
       if (w.wrapper.style.display !== 'inline') w.wrapper.style.display = 'inline';
       const max = 101;
-      const pct = Math.min(water.amount / water.goal);
+      const pct = Math.min(1, water.amount / water.goal);
       w.goal.text = `${settings.temp ? mlToOz(water.goal) : (water.goal / 1000).toFixed(1)}`;
       w.text.text = `${settings.temp ? mlToOz(water.amount) : (water.amount / 1000).toFixed(1)}`;
       w.bar.height = max - Math.round(pct * (max - 30));
